785bf82cce2bc7196b29c67780860d9dc12dbed8
Fábio Pereira <fabiopereira.desenv@gmail.com>
2023-10-26 19:12:34 -0300

    implementação de crud categoria (#2)

---
+++ src/main/java/com/tcg/lista/application/CategoriaService.java
@@ -0,0 +1,38 @@
+package com.tcg.lista.application;
+
+import com.tcg.lista.domain.categoria.Categoria;
+import com.tcg.lista.domain.categoria.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> getAllCategorias() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria getCategoriaById(Long id) {
+        return categoriaRepository.findById(id).orElse(null);
+    }
+    public Categoria createCategoria(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria updateCategoria(Long id, Categoria categoria) {
+        if (categoriaRepository.existsById(id)) {
+            categoria.setCategoria_id(id);
+            return categoriaRepository.save(categoria);
+        }
+        return null;
+    }
+
+    public void deleteCategoriaById(Long id) {
+        categoriaRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/tcg/lista/web/CategoriaController.java
@@ -0,0 +1,46 @@
+package com.tcg.lista.web;
+
+import com.tcg.lista.application.CategoriaService;
+import com.tcg.lista.domain.categoria.Categoria;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public List<Categoria> getAllListas() {
+        return categoriaService.getAllCategorias();
+    }
+
+    @GetMapping("/{id}")
+    public Categoria getCategoriaById(@PathVariable Long id) {
+        return categoriaService.getCategoriaById(id);
+    }
+
+    @Transactional
+    @PostMapping
+    public Categoria createCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.createCategoria(categoria);
+    }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public Categoria updateProduto(@PathVariable Long id, @RequestBody Categoria categoria) {
+        return categoriaService.updateCategoria(id, categoria);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public void deleteCategoria(@PathVariable Long id) {
+        categoriaService.deleteCategoriaById(id);
+    }
+}
+
